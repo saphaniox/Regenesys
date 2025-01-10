e5ef112b08ca8478b483e9c1c73cb37b6971bf8d
sammyalwayscode <olor>
2025-01-10 15:29:46 +0100

    recent update

--- Batch 4/folder structure/javascript/script.js
+++ Batch 4/folder structure/javascript/script.js
@@ -156,8 +156,131 @@ console.log("Hello Everyone");
 //   console.log("The first number is the greaest");
 // } else console.log("The second Number is the Greatest");
 
-const functionAdd = (a, b) => {
-  console.log(a + b);
-};
+// if (15 > 6) {
+//   console.log("This is True");
+// }
 
-functionAdd(2, 4);
+// if (20 < 30) {
+//   console.log("This is True");
+// } else {
+//   console.log("This is Not True");
+// }
+
+// const score = 55;
+
+// if (score === 50) {
+//   console.log("You got C");
+// } else if (score === 60) {
+//   console.log("You got B");
+// } else if (score === 70) {
+//   console.log("Great!, You got A");
+// } else {
+//   console.log("Your score is being prossed");
+// }
+
+//Functions in Javascript
+
+// function functionName (){
+//   return any value
+// }
+
+// function userName() {
+//   return "Steve Jobs";
+// }
+// console.log(userName());
+
+// Write a simple function that accepts the name of different users
+
+// function user_name(myName) {
+//   return "My name is " + myName;
+// }
+// console.log(
+//   user_name("Jacob"),
+//   user_name("Samuel"),
+//   user_name("Anisha"),
+//   user_name(3500)
+// );
+
+//Write a simple function that give the addition of two numbers
+
+// function numAdd(num1, num2, num3) {
+//   return num1 + num2 - num3;
+// }
+
+// console.log(numAdd(49, 56, 73), numAdd(34, 64, 34), numAdd(427, 364, 98));
+
+// function sayHello() {
+//   alert("Hey There, You Just clicked me");
+// }
+
+// function highlightText() {
+//   document.body.style.backgroundColor = "red";
+// }
+
+// Arrays in Javascript
+
+// const myFruits = [
+//   "Mango",
+//   "Pawpaw",
+//   "Apple",
+//   "PineApple",
+//   "Orange",
+//   4526,
+//   45673,
+// ];
+
+// myFruits[2] = "Grape";
+// myFruits[5] = "Watermellon";
+// myFruits[6] = "Cherry";
+
+// myFruits.push("Carrots");
+// myFruits.push("Guava");
+// myFruits.pop();
+
+// myFruits.unshift("Banana");
+// myFruits.unshift("Coconut");
+// myFruits.shift();
+
+// console.log(myFruits);
+
+// let subFruit = myFruits.slice(3, 6);
+// console.log(subFruit);
+
+//Loops in javascriprt
+
+// For Loop
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (let j = 1; j <= 1000; j++) {
+//   console.log(j);
+// }
+
+// let num = 1;
+// while (num <= 100) {
+//   console.log(num);
+//   num++;
+// }
+
+// let g = 0;
+// do {
+//   console.log(g);
+//   g++;
+// } while (g < 20);
+
+//Write a loop that prints out all odd numbers between 1-100
+
+// for (let numm = 12; numm <= 100; numm++) {
+//   if (numm % 2 !== 0) {
+//     console.log(numm);
+//   }
+// }
+
+//Write a loop that prints out the 2 times multiplication table from 2x1 to 2x20
+
+for (let z = 1; z <= 20; z++) {
+  console.log("2 x " + z + " = " + z * 2);
+}
+
+// Write a loop that prints out the only the even numbers of the 2times multiplication table from 2 to 60 e.g 2x2=4, 2x4=8, 2x6=12
